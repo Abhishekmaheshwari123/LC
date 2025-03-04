c0cf603eb3ce265125aeaefdff9e686925441002
Abhishekmaheshwari123 <114015344+Abhishekmaheshwari123@users.noreply.github.com>
2025-03-05 01:37:36 +0530

    Time: 16 ms (8.36%) | Memory: 40.7 MB (33.43%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,11 +1,27 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        if(n %3 != 0)n--;
-       while(n > 0){
-        if(n%3 != 0)break;
-        while(n%3 == 0) n/= 3;
-        n--;
-       }
-       return n == 0;
+        List<Integer> l = new ArrayList<>();
+        int ele = 1;
+        for(;ele <= n;){
+            l.add(ele);
+            ele *= 3;
+        }
+        return f(0,n,l);
+    //     if(n %3 != 0)n--;
+    //    while(n > 0){
+    //     if(n%3 != 0)break;
+    //     while(n%3 == 0) n/= 3;
+    //     n--;
+    //    }
+    //    return n == 0;
+    }
+    boolean f(int index , int n , List<Integer>l){
+        if(n == 0)return true;
+        if(index == l.size())return false;
+        boolean take = false ; 
+        if(n-l.get(index) >= 0) take = f(index+1 , n - l.get(index) , l);
+        boolean not = f(index+1 , n , l);
+        return take || not;
+
     }
 }
