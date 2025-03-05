095a7628cc9939e492eebe252644ed7a3e4aca62
Abhishekmaheshwari123 <114015344+Abhishekmaheshwari123@users.noreply.github.com>
2025-03-06 00:00:46 +0530

    Time: 592 ms (19.10%) | Memory: 57.5 MB (7.04%) - LeetSync

---
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> findPrimePairs(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        // if(n == 0 || n == 1)return 0;
+        int []arr = new int[1+n];
+        Arrays.fill(arr,1);
+        for(int k = 2; k*k < arr.length; k++){
+            if(arr[k] == 1){
+                int i = k;
+                int j = k;
+                while(i*j < n){
+                    arr[i*j] = 0;
+                    j++;
+                }
+            }
+        }
+        HashSet<Integer> st = new HashSet<>();
+        for(int i = 2; i < arr.length-1; i++)if(arr[i] == 1)st.add(i);
+        // System.out.println(st);
+        for(int i = 2; i <= n/2; i++)
+            if(st.contains(n - i) && st.contains(i)){
+                List<Integer> ds = new ArrayList<>();
+                ds.add(i);
+                ds.add(n-i);
+                ans.add(new ArrayList<>(ds));
+            }
+        return ans;
+    }
+}
