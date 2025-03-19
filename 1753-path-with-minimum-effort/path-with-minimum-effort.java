d25931ec22f5178e5e1c4dad2ce6c4e8cae4a8c5
Abhishekmaheshwari123 <114015344+Abhishekmaheshwari123@users.noreply.github.com>
2025-03-19 15:50:11 +0530

    Time: 57 ms (32.72%) | Memory: 45.3 MB (67.85%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minimumEffortPath(int[][] arr) {
+        int [][]dp = new int[arr.length][arr[0].length];
+        for(int i = 0 ; i < arr.length; i++){
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        dp[0][0] = 0;
+        PriorityQueue<pair> q = new PriorityQueue<>((a,b) -> a.dis - b.dis);
+        // Queue<pair> q = new LinkedList<>();
+        q.add(new pair(0,0,0));
+        int []r = {-1,0,1,0};
+        int []c = {0,1,0,-1};
+        while(!q.isEmpty()){
+            pair curr = q.remove();
+            int dis = curr.dis;
+            int row = curr.row;
+            int col = curr.col;
+            for(int i = 0; i < 4; i++){
+                int newr = row + r[i];
+                int newc = col + c[i];
+                if(newr >= 0 && newr < arr.length && newc >= 0 && newc < arr[0].length){
+                    int diff = Math.max(dis , Math.abs(arr[row][col] - arr[newr][newc]));
+                    if(diff < dp[newr][newc]){
+                        dp[newr][newc] = diff;
+                        q.add(new pair(diff , newr , newc));
+                    }           
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+}
+class pair{
+    int dis;
+    int row;
+    int col;
+    pair(int dis, int row , int col){
+        this.dis = dis;
+        this.row = row;
+        this.col = col;
+    }
+}
