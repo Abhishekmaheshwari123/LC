b28d9dd384fe63adefac1db2b4991b8579de0645
Abhishekmaheshwari123 <114015344+Abhishekmaheshwari123@users.noreply.github.com>
2025-04-27 00:19:54 +0530

    Time: 350 ms (9.98%) | Memory: 235.9 MB (5.04%) - LeetSync

---
+++ 3707-find-the-most-common-response/find-the-most-common-response.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String findCommonResponse(List<List<String>> l) {
+        HashMap<String,Integer> mp = new HashMap<>();
+        for(int i = 0; i < l.size(); i++){
+            HashSet<String> st = new HashSet<>();
+            for(int j = 0; j < l.get(i).size(); j++){
+                st.add(l.get(i).get(j));
+            }
+            for(String s : st){
+                mp.put(s , mp.getOrDefault(s , 0) + 1);
+            }
+        }
+        System.out.println(mp);
+        PriorityQueue<String> pq = new PriorityQueue<>((a,b) -> {
+           return  mp.get(b) == mp.get(a) ? a.compareTo(b) :  mp.get(b) - mp.get(a) ;
+            });
+        for(String s: mp.keySet()){
+            pq.offer(s);
+        }
+        return pq.peek();
+
+        // return "";
+    }
+}
