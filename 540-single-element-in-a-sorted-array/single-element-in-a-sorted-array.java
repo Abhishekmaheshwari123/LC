59365730357cd4bca35c79af4f1e8b1ffa6b5134
Abhishekmaheshwari123 <114015344+Abhishekmaheshwari123@users.noreply.github.com>
2025-05-24 04:55:39 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (30.03%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,12 +1,26 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-       HashMap<Integer,Integer> mp = new HashMap<>();
-       for(int i : nums){
-        mp.put(i , mp.getOrDefault(i , 0)+1);
-       }
-       for(int i : mp.keySet()){
-        if(mp.get(i) == 1)return i;
-       }
-       return nums[0];
+        int n = nums.length;
+        if(n == 1)return nums[0];
+        if(nums[0] != nums[1])return nums[0];
+        if(nums[n-2] != nums[n-1])return nums[n-1];
+        int left = 1;
+        int right = n-2;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid-1] != nums[mid] && nums[mid+1] != nums[mid])return nums[mid];
+            if(mid % 2 != 0){
+                if(nums[mid] == nums[mid-1]) left = mid + 1;
+                else right = mid - 1;
+            }
+            else{
+                if(nums[mid] == nums[mid+1])left = mid + 1;
+                else right = mid - 1;
+            }
+        }
+        return nums[right];
     }
 }
+
+// left half (even,odd) 
+// right half (odd , even)
