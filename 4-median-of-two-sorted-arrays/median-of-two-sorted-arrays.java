84e11e09e776fc307bc286d8327804a35056719f
Abhishekmaheshwari123 <114015344+Abhishekmaheshwari123@users.noreply.github.com>
2025-05-29 03:27:07 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (73.55%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,12 +1,31 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Integer> l = new ArrayList<>();
-        for(int i : nums1)l.add(i);
-        for(int i : nums2)l.add(i);
-        Collections.sort(l);
-        if(l.size() % 2 == 0){
-            return (double) (l.get(l.size()/2) + l.get(l.size()/2 - 1))/2;
+      int n = nums1.length;
+      int m = nums2.length;
+      if(n > m)return findMedianSortedArrays(nums2,nums1);
+      int left = 0;
+      int right = n;
+      int half = (n + m + 1)/2;
+      while(left <= right){
+        int mid = left + (right - left) / 2;
+        int mid2 = half - mid;
+        int l1 = Integer.MIN_VALUE;
+        int l2 = Integer.MIN_VALUE;
+        int r1 = Integer.MAX_VALUE;
+        int r2 = Integer.MAX_VALUE;
+        if(mid-1 >= 0) l1 = nums1[mid-1];
+        if(mid2-1 >= 0) l2 = nums2[mid2-1];
+        if(mid < n) r1 = nums1[mid];
+        if(mid2 < m) r2 = nums2[mid2];
+        if(l1 <= r2 && l2 <= r1){
+            if((n+m) %2 == 1)return Math.max(l1,l2);
+            return (double)((double)Math.max(l1,l2) + (double)Math.min(r1,r2)) / 2;
         }
-        return (double) l.get(l.size()/2);
+        else if(l1 < r2){
+            left = mid + 1;
+        }
+        else right = mid - 1;
+      }
+      return 0;
     }
 }
